924df7d84c1e426a718101501b1da954f046f318
Sowparna Shome <soupornoshome@gmail.com>
2025-02-08 22:40:57 +0530

    autowired field injection

--- FirstApp/src/main/java/com/example/FirstApp/Dev.java
+++ FirstApp/src/main/java/com/example/FirstApp/Dev.java
@@ -1,9 +1,14 @@
 package com.example.FirstApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+    @Autowired
+    private Laptop laptop;
     public void build(){
+
+        laptop.compile();
         System.out.println("Working on awesome project");
     }
 }
---
+++ FirstApp/src/main/java/com/example/FirstApp/Laptop.java
@@ -0,0 +1,10 @@
+package com.example.FirstApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Compiling without bugs");
+    }
+}
