d3711e4685cbec3015c416e991d5fda6336668be
Sowparna Shome <soupornoshome@gmail.com>
2025-02-08 22:42:36 +0530

    constructor injection

--- FirstApp/src/main/java/com/example/FirstApp/Dev.java
+++ FirstApp/src/main/java/com/example/FirstApp/Dev.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Component;
 public class Dev {
     @Autowired
     private Laptop laptop;
+    private Laptop _laptop;
+    public Dev(Laptop laptop){
+        this._laptop=laptop;
+    }
     public void build(){
 
         laptop.compile();
+        _laptop.compile();
         System.out.println("Working on awesome project");
     }
 }
